0bbb42a83e901c3fd8fa6521a434166375472413
Shanna-kay Cummings <shanacummings12@gmail.com>
2022-12-31 21:25:13 -0500

    JavaScript HW with functions and parameters - first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//Write a javascript code that print your name, height and country on the screen.
+
+
+function greet(name, height, country) {
+    alert(name + " is " + height + " and comes from " + country);
+    console.log('Hello ' + name);
+}
+
+greet('Shanna-kay', '5 ft 9 inches', 'Jamaica');
+
+
+//Write a function that takes three(3) parameters (name, height and country)
+//Write an alert in the function that prints out the inputs.
+//Call the funct and pass the three(3) parameter values into it.
+//Push your codes to GitHub
+
+// always put space before and after a sentence addition like "is"
+//always put values that match the parameters 
+
